9cc19d3a9b806327484ad91a1e29a6e282533627
igarcia10 <ivangarciatecnico@gmail.com>
2019-09-13 15:28:04 +0200

    implemented chat

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import socket from 'socket.io-client';
+
+interface IMessage {
+    id: string,
+    text: string
+};
 
 const StyledForm = styled.form`
     background: #000; 
@@ -33,11 +39,37 @@ const MessagesUl = styled.ul`
     }
 `;
 
-export const App: React.FC = () => (
-    <div>
-        <MessagesUl></MessagesUl>
-        <StyledForm>
-            <input /><button>Send</button>
-        </StyledForm>
-    </div>
-);
+const client = socket.connect('http://localhost:8080');
+
+export const App: React.FC = () => {
+    let [message, setMessage] = useState<string>('');
+    let [messages, setMessages] = useState<IMessage[]>([]);
+
+    const sendMessage = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        client.emit('chat_message', message);
+        setMessage('');
+    };
+
+    const handleChange = (message: string) => {
+        setMessage(message);
+    };
+
+    client.emit('username', 'Iván');
+    client.on('chat_message', (m: IMessage) => setMessages([...messages, m]));
+
+    return (
+        <div>
+            <MessagesUl>
+                {messages.map(m => <li key={m.id}>{m.text}</li>)}
+            </MessagesUl>
+            <StyledForm>
+                <input type="text" value={message} onChange={e => handleChange(e.target.value)} onKeyPress={e => {
+                    if (e.keyCode === 13) {
+                        handleChange(message);
+                    }
+                }} /><button onClick={sendMessage}>Send</button>
+            </StyledForm>
+        </div>
+    );
+};
